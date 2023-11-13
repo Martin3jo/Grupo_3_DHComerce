fbb1223cd0cb2a9b19ee5bc917d23dab727585c5
CoronelAtilio <maxi-935@hotmail.com>
2023-11-13 12:24:25 -0300

    comenzando a subir registro3

--- app.js
+++ app.js
@@ -3,12 +3,24 @@ const app = express()
 const path = require("path")
 let port = 4000;
 
-app.get('/index.html',(req,res)=>{
+app.get('/',(req,res)=>{
 	res.sendFile(path.join(__dirname,'./views/index.html'));
 });
 
 app.get('/registro.html',(req,res)=>{
 	res.sendFile(path.join(__dirname,'./views/registro.html'));
+});
+
+app.get('/usuario.html',(req,res)=>{
+	res.sendFile(path.join(__dirname,'./views/usuario.html'));
+});
+
+app.get('/detalle_producto.html',(req,res)=>{
+	res.sendFile(path.join(__dirname,'./views/detalle_producto.html'));
+});
+
+app.get('/carrito.html',(req,res)=>{
+	res.sendFile(path.join(__dirname,'./views/carrito.html'));
 });
 
 app.listen(port, ()=>{
