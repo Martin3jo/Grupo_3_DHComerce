4df6103378d69b2463e79b087217fafc5c41c527
CoronelAtilio <maxi-935@hotmail.com>
2023-11-29 16:03:55 -0300

    modificacion app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 let port = 4000;
 
-app.get("/index.html", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./views/index.html"));
 });
 
