ca5873cab9e4f6892077b95511c1b25d38b1ab2b
Martin Trejo <martin.trejo.06@gmail.com>
2023-12-05 17:30:15 -0300

    organizo el app

--- app.js
+++ app.js
@@ -1,38 +1,18 @@
 const express = require("express");
 const app = express();
+const mainRoutes = require("./routes/mainRoutes.routes");
+
 const path = require("path");
 let port = 4000;
 
-const mainRoutes = require("./routes/mainRoutes.routes");
+const carpetaPublic = path.resolve(__dirname, "./public");
+app.use(express.static(carpetaPublic));
 
 app.set("view engine", "ejs");
 app.use("/", mainRoutes);
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/index.html"));
-});
-
-app.get("/registro.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/registro.html"));
-});
-
-app.get("/usuario.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/usuario.html"));
-});
-
-app.get("/detalle_producto.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/detalle_producto.html"));
-});
-
-app.get("/carrito.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/carrito.html"));
-});
 
 app.listen(port, () => {
   console.log(`
     Nuestra app funciona en
     http://localhost:${port}`);
 });
-
-const carpetaPublic = path.resolve(__dirname, "./public");
-app.use(express.static(carpetaPublic));
