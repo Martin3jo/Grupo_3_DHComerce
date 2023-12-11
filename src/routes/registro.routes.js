e54a02b1d808a64d41b737f3654682449104fd2e
CoronelAtilio <coronelatiliomaximiliano@gmail.com>
2023-12-11 16:56:43 -0300

    consejos jose

--- src/routes/registro.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const registroController = require("../controllers/registroController");
-
-router.get('/',registroController.registro)
-
-module.exports = router;
