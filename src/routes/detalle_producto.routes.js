c3c556b416a5f13ce1a01d653cee87cebf2c407c
Martin Trejo <martin.trejo.06@gmail.com>
2023-12-12 22:21:48 -0300

    se unifican las rutas para que tengamos solo producto, index y usuario

--- src/routes/detalle_producto.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const detalle_productoController = require("../controllers/detalle_productoController");
-
-router.get('/',detalle_productoController.detalle_producto)
-
-module.exports = router;
