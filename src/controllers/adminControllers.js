557f7c22631ee8bd6cad37f2bb2d2af0364ccbf4
CoronelAtilio <coronelatiliomaximiliano@gmail.com>
2024-01-05 02:19:53 -0300

    database y multer

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,29 +1,52 @@
-let productos = [
-    {
-        nombreProducto: "coca cola",
-        descripcion: "mucho azucar",
-        categoria: "gaseosa",
-        precio: 750
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../database/productsDataBase.json');
+let productos = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+
+//admin
+let admin = (req, res) => {
+    res.render('admin/admin')
+}
+
+//crear
+let crear = (req, res) => {
+    if (req.method === 'POST') {
+        let nombreProducto = req.params.nombreProducto;
+        let productosFiltrados = productos.filter(producto => producto.nombreProducto === nombreProducto);
+
+        if (productosFiltrados.length > 0) {
+            console.log("el producto se repite");
+        } else {
+            productos.push(req.body)
+        }
+        res.redirect('/crearProducto')
+    } else {
+        res.render('admin/crearProducto', { productos })
     }
-]
+}
+
+//eliminar
+let eliminar = (req,res)=>{
+    res.render('admin/eliminarProducto')
+}
+
+//modificar
+let modificar = (req,res)=>{
+    res.render('admin/modificarProducto')
+}
+
+//Buscar
+let buscar = (req,res)=>{
+    res.render('admin/buscarProducto')
+}
 
 module.exports = {
-    admin: (req, res) => {
-        res.render('admin/admin')
-    },
-    create: (req, res) => {
-        if (req.method === 'POST') {
-            let nombreProducto = req.params.nombreProducto;
-            let productosFiltrados = productos.filter(producto => producto.nombreProducto === nombreProducto);
-    
-            if (productosFiltrados.length > 0) {
-                console.log("el producto se repite");
-            } else {
-                productos.push(req.body)
-            }
-            res.redirect('/crearProducto')
-        } else {
-            res.render('admin/crearProducto', { productos })
-        }
-    }
+    admin: admin,
+    crear: crear,
+    eliminar : eliminar,
+    modificar : modificar,
+    buscar : buscar,
     }
--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,12 +1,49 @@
 const express = require("express");
 const router = express.Router();
 const multer = require('multer')
+const path = require('path')
 const adminControllers = require("../controllers/adminControllers");
+
+//lugar donde almacenara las imagenes del formulario - MULTER
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, path.resolve(__dirname, '../../public/img/avatars'))
+    },
+    filename:(req,file,cb)=>{
+        let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
+        cb(null, fileName)
+    }
+})
+const upload = multer({storage});
+
+
+//RUTAS
 
 router.get("/admin", adminControllers.admin);
 
-router.get("/crearProducto", adminControllers.create);
+//http://localhost:4000/crearProducto
+router.get("/crearProducto", adminControllers.crear);
 
-router.post('/crearProducto/:id', adminControllers.create)
+router.post('/crearProducto', upload.single('imagenProducto'), adminControllers.crear)
 
+//http://localhost:4000/eliminarProducto
+router.get("/eliminarProducto", adminControllers.eliminar);
+
+router.post('/eliminarProducto', adminControllers.eliminar);
+
+//http://localhost:4000/modificarProducto
+router.get("/modificarProducto", adminControllers.modificar);
+
+router.post('/modificarProducto', upload.single('imagenProducto'), adminControllers.modificar);
+
+//Buscar
+router.get("/buscarProducto", adminControllers.buscar);
+
+router.post('/buscarProducto', adminControllers.buscar)
+
+
+
+
+
+//EXPORTACION
 module.exports = router;
