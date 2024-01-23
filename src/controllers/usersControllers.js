6f67e406a94cc0256d635cbcf96aa3fc4c2e5a57
CoronelAtilio <coronelatiliomaximiliano@gmail.com>
2024-01-23 01:48:13 -0300

    registro y usuario validation

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,9 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator')
+
+const usersPath = path.join(__dirname, '../database/users.json');
+let usuarios = JSON.parse(fs.readFileSync(usersPath, 'utf-8'));
 
 const usersController = {
   registro: (req, res) => {
     res.render("registro");
   },
+  registroValidation: (req, res) => {
+    let resultValidation = validationResult(req);
+    if (resultValidation.errors.length > 0) {
+      return res.render('registro', {
+        errors: resultValidation.mapped(),
+        old: req.body
+      })
+
+    } else {
+      // Obtenemos los campos del body con destructuring
+      const { nombre, apellido, usuario, password, email, fechaNac } = req.body
+      // Creamos un nuevo producto con todos los campos 
+      const newUsuario = {
+        // id: Date.now(),
+        id: uuidv4(),
+        nombre,
+        apellido,
+        usuario,
+        password,
+        email,
+        fechaNac
+      }
+
+      usuarios.push(newUsuario)
+
+      let usuariosJSON = JSON.stringify(usuarios, null, '   ')
+
+      fs.writeFileSync(usersPath, usuariosJSON)
+
+      res.redirect('/')
+
+    }
+
+  }
+  ,
   login: (req, res) => {
     res.render("usuario");
   },
@@ -15,7 +56,7 @@ const usersController = {
         errors: resultValidation.mapped(),
         old: req.body
       })
-    }else{
+    } else {
       res.render("usuario");
     }
   }
