503beb4f4edac4d346640867e88d26cd2b950262
CoronelAtilio <maxi-935@hotmail.com>
2024-03-12 15:24:47 -0300

    validacion2.0

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,19 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require('express-validator')
 
 
 //middlewares
 const guestMiddleware = require('../middlewares/guestMiddleware')
 const authMiddleware = require('../middlewares/authMiddleware')
-const loginMiddleware = require('../middlewares/loginMiddleware')
-const registroMiddleware = require('../middlewares/registroMiddleware')
+
+const registroMiddleware = [
+    body('nombre')
+        .notEmpty()
+        .withMessage('Campo Obligatorio'),
+    body('apellido')
+        .notEmpty()
+        .withMessage('Campo Obligatorio'),
+    body('dni')
+        .notEmpty()
+        .withMessage('Campo Obligatorio'),
+    body('req.body.celular')
+        .isLength({ min: 10, max: 10 })
+        .withMessage('Número de celular no válido'),
+    body('req.body.password')
+        .notEmpty()
+        .withMessage('Campo Obligatorio').bail()
+        .isLength({ min: 6 }),
+    body('req.body.password2')
+        .notEmpty()
+        .withMessage('Campo Obligatorio').bail()
+        .isLength({ min: 6 }),
+    body('req.body.email')
+        .notEmpty()
+        .withMessage('Campo Obligatorio').bail()
+        .isEmail()
+        .withMessage('Debe ser un E-mail valido'),
+    body('req.body.fechaNac')
+        .notEmpty()
+        .withMessage('Campo Obligatorio').bail()
+]
 
 
 const usersControllers = require("../controllers/usersControllers");
 
 //REGISTRO
 router.get("/registro", guestMiddleware, usersControllers.registro);
-router.post('/registro'/*, registroMiddleware*/, usersControllers.registroValidacion)
+router.post('/registro', registroMiddleware, usersControllers.registroValidacion)
 
 //LOGIN
 router.get('/login', guestMiddleware, usersControllers.login)
